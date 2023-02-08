472db1535383031f30ca7875b07d909c436fe55b
hussein <alhussien.n.g@gmail.com>
2023-02-08 15:58:36 +0200

    add cors

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
-// app.set("trust proxy", 1);
+app.set("trust proxy", 1);
 app.use(mongoSanitize());
 // app.use(helmet());
 
@@ -76,7 +76,7 @@ app.use(
       httpOnly: true,
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7,
-      secure: false,
+      secure: true,
     },
   })
 );
