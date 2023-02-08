56fe34941e5d56a20cfc21a7d8874af700488c68
hussein <alhussien.n.g@gmail.com>
2023-02-08 03:30:59 +0200

    remove helmet

--- index.js
+++ index.js
@@ -35,6 +35,19 @@ let corsOptions = {
   },
   credentials: true,
   optionsSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,OPTIONS,POST,DELETE",
+  allowedHeaders: [
+    "Access-Control-Allow-Headers",
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+    "token",
+    "Access-Control-Request-Method",
+    "Access-Control-Request-Headers",
+    "Access-Control-Allow-Credentials",
+  ],
 };
 
 app.use(cors(corsOptions));
