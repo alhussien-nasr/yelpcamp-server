45a0b2d288d3ab957704e5827edc47b20124a175
hussein <alhussien.n.g@gmail.com>
2023-02-08 15:47:20 +0200

    add cors

--- middleware.js
+++ middleware.js
@@ -36,7 +36,7 @@ module.exports.isLoggedIn = (massage) => (req, res, next) => {
   console.log("working");
   if (!req.isAuthenticated()) {
     console.log("no auth", massage);
-    return res.sendStatus(401).json({ error: massage });
+    return res.json({ error: massage });
   }
   next();
 };
