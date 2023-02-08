8637a560596c32a6f4a7ab36ad02019538dd18bb
hussein <alhussien.n.g@gmail.com>
2023-02-08 05:16:36 +0200

    set proxy

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.set("trust proxy", 1);
+app.set("trust proxy", 1);
 app.use(mongoSanitize());
 app.use(helmet());
 
